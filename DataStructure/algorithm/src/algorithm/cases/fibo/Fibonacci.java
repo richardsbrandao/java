8c7cbb486cc7da5f47c5290abd08f82d41827a2f
Richard Brandão <richardsbrandao@gmail.com>
2017-12-03 23:40:40 -0200

    fibonacci cases

---
+++ DataStructure/algorithm/src/algorithm/cases/fibo/Fibonacci.java
@@ -0,0 +1,54 @@
+package algorithm.cases.fibo;
+
+public class Fibonacci {
+
+	/**
+	 * COMPLEXITY: O
+	 * SPACE: O
+	 */
+	public long recursive(int n) {
+		if(n < 2) {
+			return n; // 1 or 0
+		}
+		return recursive(n-1) + recursive(n-2);
+	}
+
+	/**
+	 * COMPLEXITY: O(n)
+	 * SPACE: O(n)
+	 */
+	public long withVector(int n) {
+		int sizeArray = n > 1 ? n : 2;
+		Integer[] fiboList = new Integer[++sizeArray];
+		fiboList[0] = 0;
+		fiboList[1] = 1;
+		return recursionWithVector(fiboList, n, 2);
+	}
+	
+	private long recursionWithVector(Integer[] fiboList, int total, int current) {
+		if( total < current ) {
+			return fiboList[total];
+		}
+		fiboList[current] = fiboList[current-1] + fiboList[current-2];
+		return recursionWithVector(fiboList, total, ++current);
+	}
+
+	/**
+	 * COMPLEXITY: O(n)
+	 * SPACE: O(1)
+	 */
+	public long withLooping(int n) {
+		long last = 0;
+		long actual = 0;
+		for(int i = 1; i <= n; i++) {
+			if( i == 1 ) {
+				actual = 1;
+			} else {
+				actual += last;
+				last = actual - last;
+			}
+		}
+		return actual;
+	}
+	
+}
---
+++ DataStructure/algorithm/test/algorithm/cases/FibonacciTest.java
@@ -0,0 +1,39 @@
+package algorithm.cases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import algorithm.cases.fibo.Fibonacci;
+
+public class FibonacciTest {
+
+	@Test
+	public void when_0() {
+		assertEquals(0L, new Fibonacci().recursive(0));
+		assertEquals(0L, new Fibonacci().withLooping(0));
+		assertEquals(0L, new Fibonacci().withVector(0));
+	}
+	
+	@Test
+	public void when_1() {
+		assertEquals(1L, new Fibonacci().recursive(1));
+		assertEquals(1L, new Fibonacci().withLooping(1));
+		assertEquals(1L, new Fibonacci().withVector(1));
+	}
+	
+	@Test
+	public void when_2() {
+		assertEquals(1L, new Fibonacci().recursive(2));
+		assertEquals(1L, new Fibonacci().withLooping(2));
+		assertEquals(1L, new Fibonacci().withVector(2));
+	}
+	
+	@Test
+	public void when_n() {
+		assertEquals(144L, new Fibonacci().recursive(12));
+		assertEquals(144L, new Fibonacci().withLooping(12));
+		assertEquals(144L, new Fibonacci().withVector(12));
+	}
+	
+}
