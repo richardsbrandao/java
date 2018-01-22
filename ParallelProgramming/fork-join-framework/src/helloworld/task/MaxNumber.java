8cede2210303cfde3b108d8657fd8888bc553f30
Richard Brandão <richard.brandao@m4u.com.br>
2018-01-22 19:45:59 -0200

    Fixing MaxNumber fork strategy

--- ParallelProgramming/fork-join-framework/src/helloworld/task/MaxNumber.java
+++ ParallelProgramming/fork-join-framework/src/helloworld/task/MaxNumber.java
@@ -2,6 +2,7 @@ package helloworld.task;
 
 import java.util.concurrent.RecursiveTask;
 
+@SuppressWarnings("serial")
 public class MaxNumber extends RecursiveTask<Integer> {
 
 	private static final int THREADSHOLD = 2;
@@ -24,30 +25,15 @@ public class MaxNumber extends RecursiveTask<Integer> {
 	@Override
 	protected Integer compute() {
 		int range = end - start;
-		if(range <= 1) {
+		if(range < THREADSHOLD) {
 			return Math.max(numbers[start], numbers[end-1]);
 		} else {
 			int split = range / 2;
 			MaxNumber left = new MaxNumber(numbers, start, start+split);
 			left.fork();
 			MaxNumber right = new MaxNumber(numbers, start+split, end);
-			right.fork();
-			return Math.max(left.join(), right.join());
+			return Math.max(right.compute(), left.join());
 		}
-//		int length =  end - start;
-//		int max = 0;
-//		if(length < THREADSHOLD) {
-//			for(int x = start; x < end; x++) {
-//				max = numbers[x];
-//			}
-//			return max;
-//		} else {
-//			int split = length / 2;
-//			MaxNumber left = new MaxNumber(numbers, start, start+split);
-//			left.fork();
-//			MaxNumber right = new MaxNumber(numbers, start+split, end);
-//			return Math.max(right.compute(), left.join());
-//		}
 	}
 
 }
